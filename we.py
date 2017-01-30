bbd6b2e9e98567d871e04ce15e96d3fb59804900
KweeSignal <uploaddownload1995@yahoo.com>
2017-01-31 00:44:43 +0630

    Create we.py

---
+++ we.py
@@ -0,0 +1,11 @@
+mnelson:myproject mnelson$ git status
+On branch master
+
+Initial commit
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+
+    mnelson.txt
+
+nothing added to commit but untracked files present (use "git add" to track)
